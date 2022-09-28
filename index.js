5d24517a43fa306e4bb81fcb5572414b74f99393
unknown <priyanshisharma085@gmail.com>
2022-09-28 14:44:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("working");
+
+// What is the Difference between react and react native? Which one is library or framework?
+// What is the package name you are using for routing ?
+// Routing Implementation 
+// How do you pass data from parent to child ?
+// What is lazy loading in react ?
+// Difference b/w Stateful and stateless Component
+// How do you switch one component to another, Conditional Rendering ?
